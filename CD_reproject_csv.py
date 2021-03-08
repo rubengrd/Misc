9942f9faf46f79293ad93fd4558f5f41f7c28732
RGR <rubengrd@hotmail.com>
2021-03-08 10:14:35 +0100

    Update CD_reproject_csv.py
    Comments to English

--- CD_reproject_csv.py
+++ CD_reproject_csv.py
@@ -1,62 +1,56 @@
 ########################################################
-######REPROYECCION DE COORDENADAS DESDE UN FICHERO######
-######CSV Y EXPORTACION#################################
-########################################################
+######GEOGRAPHIC COORDINATES REPROJECTION###############
+##############FROM A CSV FILE###########################
 
 import geopandas as gpd
 import pandas as pd
 from shapely.geometry import Point
 from fiona.crs import from_epsg
 
-#Lectura del csv y conversion en df
-#El fichero tiene que estar en la carpeta de python para que
-# funcione. Es donde estan los modulos importados arriba
-# CAMBIAR EL NOMBRE DEL FICHERO EN CUESTION ANTES DE EJECUTAR
+#Read the shapefile from the selected path
+fp = gpd.read_file("C:\python\Taintrux_210301_testpython.csv") # Here you put your path
 
-fp = gpd.read_file("C:\python\Taintrux_210301_testpython.csv")
-
+# Set up of the GeoDataFrame and its coordinate system
 geo_data = gpd.GeoDataFrame (fp, geometry = "geometry")
 geo_data[["latitude","longitude"]] = geo_data[["latitude","longitude"]].astype("float")
-geo_data.crs = 'EPSG:4326'
+geo_data.crs = 'EPSG:4326' 
 
-# Bucle a traves de todos los elementos del df
-i=0
-for lat,lon in zip(geo_data['latitude'], geo_data['longitude']):
-    p = [lat,lon]
-    geo_data.loc[i,"geometry"] = Point(p)
-    i = i+1
-    #Generamos el punto en formato shapely y con la funcion .loc lo
-    #asignamos a la casilla de la columna geometry que corresponde
+# We loop over latitude and longitude values
+for index, rows in geo_data.iterrows():
+    p = [rows.longitude, rows.latitude]
+    geo_data.loc[index, "geometry"] = Point(p)
+    #We generate the shapely point and insert it into the dataframe
+    #This loop may not be the most performing one and you'll see that
+    #Iterrows is a deprecated method, but it does its job!!!! 
 
-# Anadimos las columnas north y east al df para anadir las coordenadas reproyectadas
+# Add two new columns, northing and easting of the projected coordinates
 geo_data["north"] = ""
 geo_data["east"] = ""
 
-# Reproyeccion al RGF93 (comprobacion de la reproyeccion)
+# Reprojection to RGF93. Choose the CRS you need!
 geo_data = geo_data.to_crs("EPSG:2154")
 
-# Rellenamos los nuevos campos con los x y reproyectados
+# Loop to insert the coordinates into the new columns
 for index1, rows1 in geo_data.iterrows():
     p1 = [rows1.geometry.x, rows1.geometry.y]
     geo_data.loc[index1, "north"] = p1[0]
     geo_data.loc[index1, "east"] = p1[1]
 
 print (geo_data)
-print (geo_data.crs)
+print (geo_data.crs) # Data check
 
-# Creacion del camino de exportacion
+# Choose the export path
 outFP = r"C:\python\tests\Tai_testpython"
 
 ####################################################################
-############EXPORTACION DE FICHERO CSV##############################
+########################EXPORT TO CSV ##############################
 ####################################################################
+#Please note that you should choose only one of the two export methods#
 geo_data.to_csv(outFP)
 
 ####################################################################
-###################### O SHAPEFILE #################################
+###################OR TO SHAPEFILE #################################
 ####################################################################
 
 #geo_data.to_file(outFP)
 print ("Done!")
-
-#FUNCIONA!
